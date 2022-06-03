2d7875a709c9bdd1023fa15a50718ac3054acaf5
刘天择-2020011880 <101265257+h2-water-Allen@users.noreply.github.com>
2022-06-03 14:51:38 +0800

    Add files via upload

---
+++ 2020011880 刘天择/第二次平时作业/myFuncs/testfunc.py
@@ -0,0 +1,14 @@
+
+def functA():
+    print("functA is being called")
+
+def functB():
+    print("functB is being called")
+
+def functC():
+    print("functC is being called")
+
+def functD():
+    print("functD is being called")
+
+
