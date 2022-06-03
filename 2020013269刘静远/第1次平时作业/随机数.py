3b5e45d30bceb7a0a17b06c1309c29b63b7f0cf4
liujy660 <3097086700@qq.com>
2022-06-03 13:04:58 +0800

    no message

---
+++ 2020013269刘静远/第1次平时作业/随机数.py
@@ -0,0 +1,29 @@
+import string
+import random
+
+para = {"num": 3, "struct": {"int": {"datarange": (0, 10000)}, "float": {"datarange": (0, 10000)},
+                             "str": {"datarange": string.ascii_uppercase, "len": 50}}}
+
+
+def structDataSampling(**kwargs):
+    result = list()
+    for index in range(0, kwargs['num']):
+        element = list()
+        for key, value in kwargs['struct'].items():
+            if key == "int":
+                it = iter(value['datarange'])
+                tmp = random.randint(next(it), next(it))
+            elif key == "float":
+                it = iter(value['datarange'])
+                tmp = random.uniform(next(it), next(it))
+            elif key == "str":
+                it = iter(value['datarange'])
+                tmp = ''.join(random.SystemRandom().choice(value['datarange']) for _ in range(8))
+            element.append(tmp)
+        result.append(element)
+    return result
+
+
+result = structDataSampling(**para)
+for item in result:
+    print(item)
