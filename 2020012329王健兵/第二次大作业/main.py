25350ca21bfe1e0d92eddfac143db5b56b6cdc1b
2020012329 王健兵 <100551831+646556341@users.noreply.github.com>
2022-06-18 17:58:13 +0800

    Create main.py

---
+++ 2020012329王健兵/第二次大作业/main.py
@@ -0,0 +1,9 @@
+from Myfuncs import funcs
+
+def getfunctions(path):
+    with open(path, "r") as f:
+        text = f.read()
+    return text.split()
+funcNames = getfunctions("text.txt")
+for functionName in funcNames:
+    eval("funcs." + functionName)()
