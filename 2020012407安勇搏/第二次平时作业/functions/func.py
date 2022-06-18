c58c178caf29e883af9ea0236bed446f4b100ad9
Yongbo An <100984869+2020012407anyongbo@users.noreply.github.com>
2022-06-18 17:00:20 +0800

    Create func.py

---
+++ 2020012407安勇搏/第二次平时作业/functions/func.py
@@ -0,0 +1,11 @@
+
+def funcA():
+    print("This is function A.")
+    return "Hello It's A."
+
+def funcB():
+    print("This is function B.")
+    return "Hello It's B."
+
+def funcC():
+    print("This is function C.")
