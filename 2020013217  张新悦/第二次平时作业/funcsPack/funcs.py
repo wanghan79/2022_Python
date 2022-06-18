1f15667bd6a3bbd92e3f8fc104097cff6c307b16
2020013217 张新悦 <88198565+zhangxy601@users.noreply.github.com>
2022-06-18 09:59:22 +0800

    Add files via upload

---
+++ 2020013217  张新悦/第二次平时作业/funcsPack/funcs.py
@@ -0,0 +1,12 @@
+def funcA():
+    print('this is funcA')
+    return "funcA complited"
+def funcB():
+    print('this is funcB')
+    return "funcB complited"
+def funcC():
+    print('this is funcC')
+    return "funcC complited"
+def funcD():
+    print('this is funcD')
+    return "funcD complited"
