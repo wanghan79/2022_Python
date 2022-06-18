a2644c2dda43854c9fe4cb25cd7d3f41e378414b
2020011866_荣成刚 <100559404+rongcg@users.noreply.github.com>
2022-06-18 23:58:25 +0800

    Add files via upload

---
+++ 2020011866 荣成刚/结课作业/Decorator/ACC.py
@@ -0,0 +1,16 @@
+# ACC= （TP+TN）/（ TP+TN+FP+FN）
+
+class DecoratorAcc:
+    def __init__(self, func):
+        self._func = func
+        self.struct = None
+
+    def __call__(self, *args, **kwargs):
+        self.struct = self._func(*args, **kwargs)
+        calculate_value = self.__calculate__()
+        print("Accuracy: {:.2%}".format(calculate_value))
+
+    def __calculate__(self):
+        tp_tn = self.struct[0]+self.struct[1]
+        length = self.struct[0] + self.struct[1] + self.struct[2] + self.struct[3]
+        return tp_tn / length
---
+++ 2020011866 荣成刚/结课作业/Decorator/__init__.py
@@ -0,0 +1,2 @@
+class Decorator:
+    pass
