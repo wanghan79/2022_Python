35b2da1e53659cef12c00aaa5c38606767bf01dd
2020012329 王健兵 <100551831+646556341@users.noreply.github.com>
2022-06-18 17:56:00 +0800

    Create function.py

---
+++ 2020012329王健兵/第二次大作业/我的真菌/function.py
@@ -0,0 +1,11 @@
+def funcA():
+    print("This is function A!")
+
+def funcB():
+    print("This is funciton B!")
+
+def funcC():
+    print("This is function C!")
+
+def funcD():
+    print("This is function D!")
