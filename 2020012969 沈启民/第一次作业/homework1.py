5d2e52d5a4001f178aa50e34f61c604b796d30e9
2020012969 沈启民 <71817526+1512825845@users.noreply.github.com>
2022-06-18 20:48:56 +0800

    作业提交
    2020012969 沈启民

---
+++ 2020012969 沈启民/第一次作业/homework1.py
@@ -0,0 +1,27 @@
+import string
+import random
+
+para = {"num":5,"struct":{"int":{"datarange":(0,100)},"float":{"datarange":(0,10000)},"str":{"datarange":string.ascii_uppercase,"len":50}}}
+
+def structDataSampling(*args, **kwargs):
+    result = list()
+    for item in range(0,kwargs['num']):
+        element = list()
+        for key,value in kwargs['struct'].items():
+            if key == "int":
+                it = iter(value['datarange'])
+                tmp = random.randint(next(it), next(it))
+            elif key == "float":
+                it = iter(value['datarange'])
+                tmp = random.uniform(next(it),next(it))
+            elif key == "str":
+                tmp = ''.join(random.SystemRandom().choice(value['datarange']) for _ in range(value['len']))
+            else:
+                break
+            element.append(tmp)
+        result.append(element)
+    return result
+
+
+
+print(structDataSampling(**para))
---
+++ 2020012969 沈启民/第二次作业/funcsPack/funclib/funcs.py
@@ -0,0 +1,12 @@
+def funcA():
+    print("This is funcA.")
+    return "FuncA completed."
+def funcB():
+    print("This is funcB.")
+    return "FuncA completed."
+def funcC():
+    print("This is funcC.")
+    return "FuncA completed."
+def funcD():
+    print("This is funcD.")
+    return "FuncA completed."
