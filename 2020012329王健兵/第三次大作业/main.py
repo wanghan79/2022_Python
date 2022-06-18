c8c4ecf5b5f6ef007dd27d9fc18b2d0c797fc008
2020012329 王健兵 <100551831+646556341@users.noreply.github.com>
2022-06-18 18:10:01 +0800

    Create main.py

---
+++ 2020012329王健兵/第三次大作业/main.py
@@ -0,0 +1,70 @@
+import math
+import random
+from functools import wraps
+
+
+
+
+def decorateWith(flag):
+    def decorator(func):
+        wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            total = len(result)
+            TP = TN = FP = FN = 0
+            for e in result:
+                if (e[0] is True) and (e[1] is True):
+                    TP += 1
+                elif (e[0] is True) and (e[1] is False):
+                    FN += 1
+                elif (e[0] is False) and (e[1] is True):
+                    FP += 1
+                elif (e[0] is False) and (e[1] is False):
+                    TN += 1
+                else:
+                    pass
+            if flag == "ACC":
+                acc = (TP + TN) / total
+                print("ACC : %f" % acc)
+            elif flag == "MCC":
+                denominator = math.sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
+                mcc = (TP * TN - FP * FN) / denominator
+                print("MCC : %f" % mcc)
+            else:
+                pass
+            return result
+        return wrapper
+    return decorator
+
+@decorateWith("MCC")
+@decorateWith("ACC")
+def generateRandom(**kargs):
+    result = list()
+    num = kargs["num"]
+    struct = kargs["struct"]
+    for i in range(num):
+        element = list()
+        for key, val in struct.items():
+            if key == "int":
+                it = iter(val["range"])
+                element.append(random.randint(next(it), next(it)))
+            elif key == "float":
+                it = iter(val["range"])
+                element.append(random.uniform(next(it), next(it)))
+            elif key =="str":
+                strRange = val["range"]
+                strLength = val["length"]
+                string = ""
+                for j in range(strLength):
+                    string+=random.choice(strRange)
+                element.append(string)
+            elif key == "bool":
+                for ith in range(val["num"]):
+                    element.append(random.choice((True, False)))
+            else:
+                element.append("unknown type")
+        result.append(element)
+    return result
+
+argument = {"num": 10000, "struct": {"bool": {"num": 2}}}
+resultList = generateRandom(**argument)
