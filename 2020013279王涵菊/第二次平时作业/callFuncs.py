47aa9f6376d69efafc7635e53d9f6aa42a2e72eb
wanghanju <101262195+wanghanju@users.noreply.github.com>
2022-06-19 20:53:48 +0800

    Add files via upload

---
+++ 2020013279王涵菊/第二次平时作业/callFuncs.py
@@ -0,0 +1,16 @@
+import functions.funs as fc
+def textFileReader(file):
+    listfile = []
+    with open(file) as f:
+        for line in f:
+            listfile.append(line)
+    f.close()
+    return listfile
+
+funcsname = textFileReader("FuncList.txt")
+
+def callFuns(func):
+    eval(func)
+
+for item in funcsname:
+    callFuns(item)
