b5aebc05ca0573da65c3dd056a5c0e53926a59f6
2020012406蒋家璇 <100655818+Hustle-24-7@users.noreply.github.com>
2022-06-20 00:37:11 +0800

    Add files via upload

---
+++ 2020012406蒋家璇/第二次平时作业/funcsPack/__init__.py
@@ -0,0 +1,7 @@
+""" 
+    @Time    : 2022/6/10 11:20
+    @Author  : Jiaxuan Jiang
+    @Email   : 6812624@qq.com
+    @File    : __init__.py.py
+    @Software: PyCharm
+"""
---
+++ 2020012406蒋家璇/第二次平时作业/funcsPack/funcs.py
@@ -0,0 +1,23 @@
+""" 
+    @Time    : 2022/6/10 11:21
+    @Author  : Jiaxuan Jiang
+    @Email   : 6812624@qq.com
+    @File    : funcs.py
+    @Software: PyCharm
+"""
+
+
+def funcA():
+    print("funcA is called successfully")
+
+
+def funcB():
+    print("funcB is called successfully")
+
+
+def funcC():
+    print("funcC is called successfully")
+
+
+def funcD():
+    print("funcD is called successfully")
