cf1599f5d1796c610397afcc1d71e817c0227032
2020011882朱晨虹 <101263060+zzhuchenhong@users.noreply.github.com>
2022-06-19 15:30:58 +0800

    Add files via upload

---
+++ 2020011882朱晨虹/平时作业三/decorator.py
@@ -0,0 +1,92 @@
+"""
+li
+pass
+2022 5/25
+"""
+import math
+import random
+
+
+def ACC():
+    def dec(func):
+        def wrapper(*args, **kwargs):
+            tmplst = list(*args)
+            tp = fp = fn = tn = 0
+            for item in tmplst:
+                if item[0] == 1 & item[1] == 1:
+                    tp = tp + 1
+                elif item[0] == 1 & item[1] == 0:
+                    fp = fp + 1
+                elif item[0] == 0 & item[1] == 1:
+                    fn = fn + 1
+                elif item[0] == 0 & item[1] == 0:
+                    tn = tn + 1
+            acc = (tp + tn) / len(tmplst)  # 计算ACC
+            print("ACC:", acc)             # 打印ACC
+            return func(*args, **kwargs)
+        return wrapper
+    return dec
+
+
+def MCC():
+    def dec(func):
+        def wrapper(*args, **kwargs):
+            tmplist = list(*args)
+            tp = fp = fn = tn = 0
+            for item in tmplist:
+                if item[0] == 1 & item[1] == 1:
+                    tp = tp + 1
+                elif item[0] == 1 & item[1] == 0:
+                    fp = fp + 1
+                elif item[0] == 0 & item[1] == 1:
+                    fn = fn + 1
+                elif item[0] == 0 & item[1] == 0:
+                    tn = tn + 1
+            t = (tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)
+            mcc = (tp * tn - fp * fn) / math.sqrt(t)  # 计算MCC
+            print("MCC:", mcc)                        # 打印ACC
+            return func(*args, **kwargs)
+        return wrapper
+    return dec
+
+
+def dataSampling(num, **kwargs):
+    '''
+
+    :param struct:
+    :return: 随机生成num个0，1二元数组
+    '''
+    result = list()
+    for i in range(0, num):
+        element = list()
+        for key, value in kwargs.items():
+            if key == 'int':
+                it = iter(value['datarange'])
+                tmp = random.randint(next(it), next(it))
+            elif key == 'float':
+                it = iter(value['datarange'])
+                tmp = random.uniform(next(it), next(it))
+            elif key == 'str':
+                tmp = ''.join(random.SystemRandom().choice(value['datarange']) for _ in range(value['len']))
+            elif key == 'bool':
+                for t in range(0,2):
+                    it = iter(value['datarange'])
+                    tmp = bool(random.randint(next(it), next(it)))
+                    element.append(tmp)
+            else:
+                break
+        result.append(element)
+    # print(result, len(result))
+    return result
+
+@ACC()
+@MCC()
+def pred(recieve):
+    return recieve
+
+
+struct = {"bool":{"datarange":(0,2)}}
+reclist = dataSampling(100000, **struct)
+pred(reclist)
+
+
---
+++ 2020011882朱晨虹/平时作业二/funcsPack/funcs.py
@@ -0,0 +1,16 @@
+"""
+li
+pass
+2022 5/6
+"""
+def funcA():
+    print("this is functionA")
+
+def funcB():
+    print("this is functionB")
+
+def funcC():
+    print("this is dunctionC")
+
+def funcD():
+    print("this is functionD")
