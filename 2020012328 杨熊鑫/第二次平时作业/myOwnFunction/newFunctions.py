9f2fa75b6e24f25f836b2276328491a94d6bce86
2020012328_杨熊鑫 <100661231+kidult-sanjin@users.noreply.github.com>
2022-06-20 17:10:20 +0800

    Add files via upload

---
+++ 2020012328 杨熊鑫/第二次平时作业/myOwnFunction/newFunctions.py
@@ -0,0 +1,14 @@
+def funcA():
+    print("This is function A")
+
+
+def funcB():
+    print("This is function B")
+
+
+def funcC():
+    print("This is function C")
+
+
+def funcD():
+    print("This is function D")
