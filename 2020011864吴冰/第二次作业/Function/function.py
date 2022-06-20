86720ef3348c70710ed730de54da530bdddcbcb0
2020011864吴冰 <100567935+icy-orange-icy@users.noreply.github.com>
2022-06-20 13:52:51 +0800

    Create function.py

---
+++ 2020011864吴冰/第二次作业/Function/function.py
@@ -0,0 +1,14 @@
+def functionA():
+    print("This is function A!")
+
+
+def functionB():
+    print("This is funciton B!")
+
+
+def functionC():
+    print("This is function C!")
+
+
+def functionD():
+    print("This is function D!")
