b0293d8044abdcc907f6c61145e2030d32cde406
mengyishi <100558404+mengyishi@users.noreply.github.com>
2022-06-20 21:41:07 +0800

    Update homework1.py

--- 2020012437 郝帅/homework1/homework1.py
+++ 2020012437 郝帅/homework1/homework1.py
@@ -1,1 +1,28 @@
 
+import random
+import string
+
+def Node(**text):
+    num = test['num']
+    ans = list()
+    for i in range(0,num):
+        res = list()
+        for j,k in test['txt'].items():
+            if j == 'int':
+                t = iter(k['Data'])
+                tmp = random.randint(next(t),next(t))
+            elif j == 'float':
+                t = iter(k['Data'])
+                tmp = random.uniform(next(t),next(t))
+            elif j == 'str':
+                tmp = ''.join(random.SystemRandom().choice(k['Data']) for _ in range(value['cnt'])))
+            else:
+                break
+            res.append(tmp)
+        ans.append(res)
+    return ans
+
+file = open("hw1.txt",'r')
+rf = file.read()
+f = eval(rf)
+printf(Node(**f))
