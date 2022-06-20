84da8d884fcedd045be4550fb6ff081186432097
2020012611姜姗杉 <100561330+2020012611@users.noreply.github.com>
2022-06-20 10:05:01 +0800

    Add files via upload

---
+++ 2020012611-姜姗杉/第二次平时作业.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import funcs
+
+
+def txtFileReader(file):
+    lstfiles = []
+    with open(file) as f:
+        for line in f:
+            lstfiles.append(line)
+    f.close()
+    return lstfiles
+
+
+func = txtFileReader("./config.txt")
+# print(func)
+
+
+def evaloutsidecall(funcNames):
+    for item in funcNames:
+        eval(item)
+    # result = eval(funcName)
+    # return result
+
+evaloutsidecall(func)
+# for item in func:
+#     evaloutsidecall(item)
