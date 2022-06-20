b8c8b5592ae4caac065dc2f10fdad93f31363f17
Tarkee <2200295996@qq.com>
2022-06-20 23:52:56 +0800

    Update homework3.py

--- 2020012283 李瑞颜/第三次作业/homework3.py
+++ 2020012283 李瑞颜/第三次作业/homework3.py
@@ -1,118 +1,124 @@
-"""
-    Author  : Ruiyan Li
-    File    :
-    describe: 实现2个修饰器ACC和MCC，对模拟二分类预测结果的精度和马修相关系数进行计算，其中模拟预测结果采用随机数生成函数作为被修饰函数
-"""
 import random
 import math
-from functools import wraps
 
 
-def Decorator(level):
+class ResultAnalysis:
+    def __init__(self, func, static="ACC"):
+        self.__func = func
+        self.__static = static
 
-    # 1.ACC计算方法
-    def ACC(data):
+    def __call__(self, *args, **kwargs):
+        results = self.__func(*args, **kwargs)
+        if results is None:
+            return None
+        if self.__static == "ACC":
+            return self.ACC(results)
+        elif self.__static == "MCC":
+            return self.MCC(results)
+        else:
+            pass
+    # 计算ACC
+    def ACC(self, data):
         correct = 0
         sum = len(data)
-        for i, element in enumerate(data):  # 使用enumerate生成索引序列，同时列出数据和数据下标
-            if element[0] == element[1]:
+        for index, sample in enumerate(data): # 使用enumerate生成索引序列，同时列出数据和数据下标
+            if sample[0] == sample[1]:
                 correct += 1
-        accuracy = correct / sum
-        print("ACC =%f" % accuracy)
+        ans = correct / sum
 
-    # 2.MCC计算方法
-    def MCC(data):
+        return ans
+    # 计算MCC
+    def MCC(self, data):
         TP = 0
         TN = 0
         FP = 0
         FN = 0
-        for i, element in enumerate(data):
-            if element[0] == 1 and element[1] == 1:
+        for index, sample in enumerate(data):
+            if sample[0] == 1 and sample[1] == 1:
                 TP += 1
-            elif element[0] == 1 and element[1] == 0:
+            elif sample[0] == 1 and sample[1] == 0:
                 FN += 1
-            elif element[0] == 0 and element[1] == 1:
+            elif sample[0] == 0 and sample[1] == 1:
                 FP += 1
-            elif element[0] == 0 and element[1] == 0:
+            elif sample[0] == 0 and sample[1] == 0:
                 TN += 1
         denominator = math.sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
         if denominator == 0:
             ans = 0
         else:
             ans = (TP * TN - FP * FN) / denominator
-        print("MCC = %f" %ans)
 
-    # 3. 嵌套函数，在得到原函数的结果后，根据level为ACC或MCC进行响应的计算并得出结果
-    def wrap1(func):
-        @wraps(func)
-        def wrap2(**kwargs):
-            ans = func(**kwargs)
-
-            if level == "ACC":
-                ACC(ans)
-            else :
-                MCC(ans)
-        return wrap2
-    return wrap1
+        return ans
 
 
-@Decorator("ACC")
+# 函数修饰器,用来传入static的值
+def Result_Analysis(static):
+    def wrapper(func):
+        return ResultAnalysis(func, static)
+
+    return wrapper
+
+
+
+@Result_Analysis("ACC")
 def structDataSampling_ACC(**kwargs):
+    """
+    :param kwargs:
+    :return:
+    """
     result = list()
-    for index in range(0, kwargs['num']):
-
+    for index in range(kwargs['num']):
         for key, value in kwargs['struct'].items():
-            if key == 'int':
-                it = iter(value['datarange'])
+            if key == "int":
+                it = iter(value['dataRange'])
                 tmp = random.randint(next(it), next(it))
-            elif key == 'float':
-                it = iter(value['datarange'])
+            elif key == "float":
+                it = iter(value['dataRange'])
                 tmp = random.uniform(next(it), next(it))
-            elif key == 'str':
-                tmp = ''.join(random.choice(value['datarange']) for _ in range(value['len']))
+            elif key == "str":
+                it = iter(value['dataRange'])
+                tmp = ''.join(random.SystemRandom().choice(value['dataRange']) for _ in range(8))
             elif key == "bool":
                 tmp = []
                 for i in range(value['number']):
-                    tmp.append(random.choice((0, 1)))
-
+                    tmp.append(random.choice((True, False)))
                 tmp = tuple(tmp)
-
             else:
                 break
             result.append(tmp)
-
     return result
 
 
-@Decorator("MCC")
+@Result_Analysis("MCC")
 def structDataSampling_MCC(**kwargs):
+
     result = list()
-    for index in range(0, kwargs['num']):
-        element = list()
+    for index in range(kwargs['num']):
         for key, value in kwargs['struct'].items():
-            if key == 'int':
-                it = iter(value['datarange'])
+            if key == "int":
+                it = iter(value['dataRange'])
                 tmp = random.randint(next(it), next(it))
-            elif key == 'float':
-                it = iter(value['datarange'])
+            elif key == "float":
+                it = iter(value['dataRange'])
                 tmp = random.uniform(next(it), next(it))
-            elif key == 'str':
-                tmp = ''.join(random.choice(value['datarange']) for _ in range(value['len']))
+            elif key == "str":
+                it = iter(value['dataRange'])
+                tmp = ''.join(random.SystemRandom().choice(value['dataRange']) for _ in range(8))
             elif key == "bool":
                 tmp = []
                 for i in range(value['number']):
-                    tmp.append(random.choice((0, 1)))
+                    tmp.append(random.choice((True, False)))
                 tmp = tuple(tmp)
-
             else:
                 break
-            element.append(tmp)
-        result.append(element)
+            result.append(tmp)
     return result
 
 
-para = {"num": 10000, "struct": {"bool": { "number": 2}}}
+para = {"num": 5000, "struct": {"bool": { "number": 2}}}
 
-structDataSampling_ACC(**para)
+accAns = structDataSampling_ACC(**para)
+print(accAns)
 
-mccResult = structDataSampling_MCC(**para)
+mccAns = structDataSampling_MCC(**para)
+print(mccAns)
